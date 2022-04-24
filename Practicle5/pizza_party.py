90056dcfecac463fdeb4563d33d7a372cdb5af6f
JasonChen2118 <98323610+JasonChen2118@users.noreply.github.com>
2022-04-24 21:58:24 +0800

    Update pizza_party.py
    Add some change according to the feedback

--- Practicle5/pizza_party.py
+++ Practicle5/pizza_party.py
@@ -6,9 +6,10 @@
 # while (True)
 #   calculate the slices under certain cuts
 #   cuts++
-#   print slices number
+#   print slices and cuts number
 #   if slices >= 64
 #       break
+# prints results
 
 cuts = 1
 slices = 0
@@ -16,9 +17,10 @@ while True:
 # I set the condition as "True" to make an "infinite" loop until it meets the command "break".
     slices = (cuts ** 2 + cuts + 2) / 2
     cuts = cuts + 1
-    print(slices)
     if slices >= 64:
         break
         # Now it's time to jump out of this loop.
+print("cuts:",cuts,"slices:", slices)
+
 
 # I wonder why the output numbers include fractional parts like 64.0.
