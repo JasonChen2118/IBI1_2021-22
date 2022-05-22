91c8380f297b33f6673cc15df83bc4b526f5ac4e
JasonChen2118 <98323610+JasonChen2118@users.noreply.github.com>
2022-05-22 17:01:14 +0800

    Update Chocolate_bar_affordability_calculator.py

--- Practical10/Chocolate_bar_affordability_calculator.py
+++ Practical10/Chocolate_bar_affordability_calculator.py
@@ -1,11 +1,25 @@
 def chocolate_number(total, price):
     count = int(total / price)
-    return count
+    change = int(total % price)
+    return count, change
+    # return the number of bars that can be bought and the change
 
 
-total_money = float(input("total money:"))
-price = float(input("price:"))
+total_money = int(input("total money:"))
+price = int(input("price:"))
+# suppose that money and price are simple numeric variables
 
-print(chocolate_number(total_money, price))
+result = chocolate_number(total_money, price)
+print("bars:", result[0])
+print("change:", result[1])
 
+"""
+Example:
+input:
+total money:20
+price:7
 
+output:
+bars: 2
+change: 6
+"""
